04ae74f6e2349b80f0cc481a7fb0a7d33a3fcbf5
shama <shama@SHAMANLAP>
2025-11-25 15:26:06 -0700

    cambion en db.js intencio del cambio que solo requiera una variable de etorno la datbase_url

--- db.js
+++ db.js
@@ -1,16 +1,27 @@
 // db.js
 const { Pool } = require('pg');
 
+//const pool = new Pool({
+//  host: process.env.DB_HOST,
+//  port: process.env.DB_PORT,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD,
+//  database: process.env.DB_NAME,
+//  ssl: process.env.NODE_ENV === 'production'
+//    ? { rejectUnauthorized: false } // Render requiere SSL
+//    : false // en local no necesitas SSL
+//});
+
+// db test en caso de error capaz y es culpa de esto
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false } // Render requiere SSL
-    : false // en local no necesitas SSL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // necesario para Render
+    }
 });
 
 module.exports = pool;
+
+
 
