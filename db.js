cf7b3b19bea222c236a4f57beee160c8a2f46a35
shama <shama@SHAMANLAP>
2025-11-25 16:43:35 -0700

    arreglo de prueba

--- db.js
+++ db.js
@@ -1,35 +1,27 @@
 // db.js
 
+const { Pool } = require('pg');
+//const dotenv = require('dotenv');
+
+//dotenv.config();
 
 //const pool = new Pool({
-//  host: process.env.DB_HOST,
-//  port: process.env.DB_PORT,
 //  user: process.env.DB_USER,
-//  password: process.env.DB_PASSWORD,
+//  host: process.env.DB_HOST,
 //  database: process.env.DB_NAME,
-//  ssl: process.env.NODE_ENV === 'production'
-//    ? { rejectUnauthorized: false } // Render requiere SSL
-//    : false // en local no necesitas SSL
+//  password: process.env.DB_PASSWORD,
+//  port: process.env.DB_PORT,
+//  ssl: {
+//    rejectUnauthorized: false // Render necesita SSL
+//  }
 //});
 
-// db test en caso de error capaz y es culpa de esto
-
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false // Render necesita SSL
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // necesario para Render
+    }
 });
-
 module.exports = pool;
 
 
