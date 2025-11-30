8c99c6db51ebe26d16ea627032450e1e53c9c8d9
l21330457 <l21330457@hermosillo.tecnm.mx>
2025-11-30 12:46:41 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,13 @@ export default function Navbar() {
   return (
     <nav style={{ padding: "1rem", backgroundColor: "#eee" }}>
       <Link to="/" style={{ marginRight: 10 }}>Home</Link>
-      <Link to="/catalogo">Catálogo</Link>
+      <Link to="/Disponibilidad">Disponibilidad</Link>
+      <Link to="/Envios">Envios</Link>
+      <Link to="/Gestion">Gestion</Link>
+      <Link to="/Venta">Venta</Link>
     </nav>
   );
 }
+
 
 
