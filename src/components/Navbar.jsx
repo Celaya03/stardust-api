46e5d226e71593ef96de9578abf498761f4e938f
l21330457 <l21330457@hermosillo.tecnm.mx>
2025-11-30 13:12:09 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "1rem", backgroundColor: "#eee" }}>
-      <Link to="/" style={{ marginRight: 10 }}>Home</Link>
-      <Link to="/Disponibilidad">Disponibilidad</Link>
-      <Link to="/Envios">Envios</Link>
-      <Link to="/Gestion">Gestion</Link>
-      <Link to="/Venta">Venta</Link>
+    <nav>
+      <div className="nav-links">
+        <Link to="/" style={{ marginRight: 0 }}>Home</Link>
+        <Link to="/Disponibilidad">Disponibilidad</Link>
+        <Link to="/Envios">Envios</Link>
+        <Link to="/Gestion">Gestion</Link>
+        <Link to="/Venta">Venta</Link>
+      </div>
     </nav>
   );
 }
+
 
 
 
