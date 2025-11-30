89660bbf9028257d808d4267ec789fc1d3bd34b7
l21330457 <l21330457@hermosillo.tecnm.mx>
2025-11-29 22:01:30 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [ react() ],
-  // Opcional: cambiar alias para imports
   resolve: {
     alias: {
       '@': '/src'
     }
-  },
-  // Opcional: configuración para servidor de desarrollo
-  server: {
-    port: 5173,
-    // host: '0.0.0.0', // usar si necesitas acceso desde red externa
   }
 })
+
