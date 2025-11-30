f552bd9dc45a4f4bd17e03b6345e9cad7da1fac6
l21330457 <l21330457@hermosillo.tecnm.mx>
2025-11-29 22:39:14 -0700

    Update Catalogo.jsx

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,25 +1,8 @@
-import { useEffect, useState } from "react";
-import { apiGet } from "../services/api";
-
 export default function Catalogo() {
-  const [productos, setProductos] = useState([]);
-
-  useEffect(() => {
-    apiGet("/catalogo")
-      .then(setProductos)
-      .catch(() => alert("Error cargando catálogo"));
-  }, []);
-
   return (
     <div style={{ padding: 20 }}>
-      <h2>Catálogo de Productos</h2>
-      <ul>
-        {productos.map(p => (
-          <li key={p.id_producto}>
-            {p.nombre} — ${p.precio} — Stock: {p.stock}
-          </li>
-        ))}
-      </ul>
+      <h2>Catálogo de productos</h2>
+      <p>(Aquí iría la lista de productos...)</p>
     </div>
   );
 }
