bc22c48306ed8213de54a2360c7e3b0fa8d7dbc8
Celaya03 <celayaj310@gmail.com>
2025-12-08 17:55:05 -0700

    env

--- routes/venta.js
+++ routes/venta.js
@@ -110,7 +110,7 @@ router.post('/producto', async (req, res) => {
     await pool.query(
       `INSERT INTO edo_env (id_orden_externa, codigo_seguimiento, estado_actual, ubicacion_actual, fecha_actualizacion)
        VALUES ($1,$2,$3,$4,NOW())
-       ON CONFLICT (id_orden_externa) DO UPDATE
+       ON CONFLICT (codigo_seguimiento) DO UPDATE
          SET codigo_seguimiento = EXCLUDED.codigo_seguimiento,
              estado_actual = EXCLUDED.estado_actual,
              ubicacion_actual = EXCLUDED.ubicacion_actual,
